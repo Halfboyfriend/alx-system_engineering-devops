1c53c7ec3e7d0ccc680ddd53264766822182a8f2
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2023-03-25 03:03:21 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet, install flask from pip3
 
-package { 'Puppet-lint':
-	ensure => '2.1.0',
-	provider => 'gem',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
